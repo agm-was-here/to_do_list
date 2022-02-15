ca3cbb1a2dce25f076d7f72cd5662ba6061c8b60
agm_was_here <aswinmenon92@gmail.com>
2022-02-16 02:08:41 +0530

    basic skeletal structure

--- appfile/urls.py
+++ appfile/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('frontpage',views.frontpage,name='frontpage'),
+    path('disp', views.disp, name='disp'),
 ]
--- appfile/views.py
+++ appfile/views.py
@@ -5,4 +5,12 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'home.html',{'name' :'django'})
+    return render(request, 'home.html',{'name' :'james'})
+
+def frontpage(request):
+    return render(request, 'frontpage.html')
+
+def disp(request):
+    disp_task = request.GET['task1']
+    disp_desc=request.GET['desc1']
+    return render(request, 'display.html',{'task' : disp_task},{'desc' : disp_desc})
